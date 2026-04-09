d76c97404e213f71dfbf3488f9d6eee9fba15820
Vivek Neelam <vivek.neelam2012@gmail.com>
2026-04-09 16:36:32 -0700

    added capsys to function

--- duplicateListpytest.py
+++ duplicateListpytest.py
@@ -1,6 +1,38 @@
-import pytest
+
+import sys
 from duplicateList import getListDuplicates
 
+def test_getListDuplicates(capsys):
+
+    getListDuplicates([6,7,6,7,8,9])
+    captured = capsys.readouterr()
+    assert [6,7,6,7,8,9] in captured.out
+    
+    getListDuplicates([3,2,4,5,6])
+    captured = capsys.readouterr()
+    assert [3,2,4,5,6] in captured.out
+
+    getListDuplicates([4,4,5,6,7])
+    captured = capsys.readouterr()
+    assert [4,4,5,6,7] in captured.out
+
+    getListDuplicates([])
+    captured = capsys.readouterr()
+    assert [] in captured.out
+
+    getListDuplicates([1])
+    captured = capsys.readouterr()
+    assert [1] in captured.out
+
+    getListDuplicates([1,2])
+    captured = capsys.readouterr()
+    assert [1,2] in captured.out
+
+    getListDuplicates([2,2])
+    captured = capsys.readouterr()
+    assert [2,2] in captured.out
+
+'''
 test_getListDuplicates = [
     ([6,7,6,7,8,9], [6,7]),
     ([3,2,4,5,6], []),
@@ -18,3 +50,4 @@ def test_is_reverseString(string1, expected):
 if __name__ == "__main__":
     print("Running pytest test cases...\n")
     pytest.main([__file__]) 
+'''
