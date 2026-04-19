9d715e73b819839f396262ba42ceaacb303eaec6
Vivek Neelam <vivek.neelam2012@gmail.com>
2026-04-19 15:55:02 -0700

    problem 12 program and pytest; Program that checks a string and returns all 3 digit codes

---
+++ detectiveProgrampytest.py
@@ -0,0 +1,24 @@
+import sys 
+import pytest
+from detectiveProgram import detectiveProgram 
+
+data_detectiveProgram = [
+    (["hello077hello"], "077"),
+    (["hellohihey"], ""),
+    (["hello89fifty"], ""),
+    (["hello4567"], "456\n567"),
+    (["h670i673"], "670\n673"),
+    (["h670673i"], "670\n706\n067\n673")
+]
+
+@pytest.mark.parametrize("input, expected_output", data_detectiveProgram)
+def test_getListDuplicates(capsys, input, expected_output):
+    detectiveProgram(input)
+    captured = capsys.readouterr()
+    assert expected_output in captured.out
+
+if __name__ == "__main__":
+    print("Running pytest test cases...\n")
+    pytest.main([__file__]) 
+
+    
