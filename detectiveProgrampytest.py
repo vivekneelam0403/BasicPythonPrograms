356243bc69a57b982700a11a0e4ab8631893e7e9
Vivek Neelam <vivek.neelam2012@gmail.com>
2026-04-19 17:07:24 -0700

    Added edge cases to program and pytest

--- detectiveProgrampytest.py
+++ detectiveProgrampytest.py
@@ -8,7 +8,10 @@ data_detectiveProgram = [
     (["hello89fifty"], ""),
     (["hello4567"], "456\n567"),
     (["h670i673"], "670\n673"),
-    (["h670673i"], "670\n706\n067\n673")
+    (["h670673i"], "670\n706\n067\n673"),
+    ([""], ""),
+    (["56788"], "567\n678\n788")
+    (["hellonocde678"], "678")
 ]
 
 @pytest.mark.parametrize("input, expected_output", data_detectiveProgram)
